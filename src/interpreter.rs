b32631253d71ec082e3eb1de286afc69f1ea328f
sudeep <sudeepdino008@gmail.com>
2023-03-09 21:42:11 +0530

    simplifies imports

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -9,17 +9,17 @@ pub struct Interpreter {}
 #[derive(Debug)]
 pub enum IResult {
     Number(f64),
-    String(String),
+    String(std::string::String),
     Bool(bool),
 }
-
+use crate::interpreter::IResult::{Bool, Number, String};
 impl Visitor<IResult> for Interpreter {
     fn visit_literal(&self, lit: &Literal) -> IResult {
         match &lit.value.ttype {
-            TokenType::String(contents) => IResult::String(contents.to_string()),
-            TokenType::Number(value) => IResult::Number(*value),
-            TokenType::True => IResult::Bool(true),
-            TokenType::False => IResult::Bool(false),
+            TokenType::String(contents) => String(contents.to_string()),
+            TokenType::Number(value) => Number(*value),
+            TokenType::True => Bool(true),
+            TokenType::False => Bool(false),
             tkn => self.error(&tkn, "invalid token found; expected literal"),
         }
     }
@@ -36,8 +36,8 @@ impl Visitor<IResult> for Interpreter {
                 } else {
                     1.0
                 };
-                if let IResult::Number(value) = self.visit_expression(&unr.expr) {
-                    IResult::Number(minus * value)
+                if let Number(value) = self.visit_expression(&unr.expr) {
+                    Number(minus * value)
                 } else {
                     self.error(
                         &unr.operator.ttype,
@@ -46,8 +46,8 @@ impl Visitor<IResult> for Interpreter {
                 }
             }
             TokenType::Bang => {
-                if let IResult::Bool(value) = self.visit_expression(&unr.expr) {
-                    IResult::Bool(!value)
+                if let Bool(value) = self.visit_expression(&unr.expr) {
+                    Bool(!value)
                 } else {
                     self.error(&unr.operator.ttype, "invalid operand for bang operator");
                 }
@@ -61,54 +61,54 @@ impl Visitor<IResult> for Interpreter {
         let rightv = self.visit_expression(&bin.right);
         match &bin.operator.ttype {
             TokenType::Plus => {
-                if let IResult::Number(left) = leftv {
-                    if let IResult::Number(right) = rightv {
-                        return IResult::Number(left + right);
+                if let Number(left) = leftv {
+                    if let Number(right) = rightv {
+                        return Number(left + right);
                     }
                 }
-                if let IResult::String(left) = leftv {
-                    if let IResult::String(right) = rightv {
-                        return IResult::String(format!("{}{}", left, right));
+                if let String(left) = leftv {
+                    if let String(right) = rightv {
+                        return String(format!("{}{}", left, right));
                     }
                 }
                 self.error(&bin.operator.ttype, "invalid operands for plus operator");
             }
             TokenType::Minus => {
-                if let IResult::Number(left) = leftv {
-                    if let IResult::Number(right) = rightv {
-                        return IResult::Number(left - right);
+                if let Number(left) = leftv {
+                    if let Number(right) = rightv {
+                        return Number(left - right);
                     }
                 }
                 self.error(&bin.operator.ttype, "invalid operands for minus operator");
             }
             TokenType::Star => {
-                if let IResult::Number(left) = leftv {
-                    if let IResult::Number(right) = rightv {
-                        return IResult::Number(left * right);
+                if let Number(left) = leftv {
+                    if let Number(right) = rightv {
+                        return Number(left * right);
                     }
                 }
                 self.error(&bin.operator.ttype, "invalid operands for star operator");
             }
             TokenType::Slash => {
-                if let IResult::Number(left) = leftv {
-                    if let IResult::Number(right) = rightv {
-                        return IResult::Number(left / right);
+                if let Number(left) = leftv {
+                    if let Number(right) = rightv {
+                        return Number(left / right);
                     }
                 }
                 self.error(&bin.operator.ttype, "invalid operands for slash operator");
             }
             TokenType::Greater => {
-                if let IResult::Number(left) = leftv {
-                    if let IResult::Number(right) = rightv {
-                        return IResult::Bool(left > right);
+                if let Number(left) = leftv {
+                    if let Number(right) = rightv {
+                        return Bool(left > right);
                     }
                 }
                 self.error(&bin.operator.ttype, "invalid operands for greater operator");
             }
             TokenType::GreaterEqual => {
-                if let IResult::Number(left) = leftv {
-                    if let IResult::Number(right) = rightv {
-                        return IResult::Bool(left >= right);
+                if let Number(left) = leftv {
+                    if let Number(right) = rightv {
+                        return Bool(left >= right);
                     }
                 }
                 self.error(&bin.operator.ttype, "invalid operands for greater-equal")
